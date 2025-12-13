cf4d348124be607c3d4e7bc7f8a7497aa0a78c1e
Jiaqing Guan <guan.412@osu.edu>
2025-12-13 17:23:41 -0500

    Add files via upload

---
+++ Proj2/src/Main.java
@@ -0,0 +1,40 @@
+import project2.CoreScanner;
+import project2.ParserException;
+import project2.SemanticChecker;
+import project2.SemanticException;
+import project2.Node.ProcedureNode;
+import project2.Parser.Parser;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            if (args.length != 1) {
+                System.out.println("Usage: java -jar CoreInterpreter.jar <source-file>");
+                return;
+            }
+            String filePath = args[0];
+
+            CoreScanner scanner = new CoreScanner(filePath);
+
+            Parser parser = new Parser(scanner);
+
+            try {
+                ProcedureNode root = parser.parseProcedure();
+                SemanticChecker checker = new SemanticChecker();
+                checker.check(root);
+                System.out.println(root.toPrint(""));
+            } catch (ParserException e) {
+                System.out.println(e.getMessage());
+                return;
+            } catch (SemanticException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+
+        } catch (Exception e) {
+
+            System.out.println("ERROR: Unexpected error - " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
